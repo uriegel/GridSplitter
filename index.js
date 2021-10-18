99fe96c15ee9cdcc8abcbe238d0833ae047e74aa
uriegel <uriegel@hotmail.de>
2021-10-18 21:38:59 +0200

    Theming

--- index.js
+++ index.js
@@ -10,14 +10,11 @@ themeChooser.onchange = () => {
     const changeTheme = theme => {
         ["themeBlue", "themeAdwaita", "themeAdwaitaDark"].forEach(n => {
             document.body.classList.remove(n)    
-            table.classList.remove(n)    
         })
         document.body.classList.add(theme)    
         const style = getComputedStyle(document.body)
         exifColor = style.getPropertyValue('--exif-color') 
         selectedExifColor = style.getPropertyValue('--selected-exif-color') 
-        table.classList.add(theme)    
-        table.themeChanged()
     }
 
     switch (themeChooser.selectedIndex) {
